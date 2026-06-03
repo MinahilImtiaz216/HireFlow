77d7e22d5d3a268a07ac3c00ad3affe97823b1de
XYPHER <mughalminahil217@gmail.com>
2026-06-04 00:14:59 +0500

    Update frontend with HireFlow branding and UI improvements

--- backend/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RecruitmentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7667555daa3f273e57fe16962f898bce79242692")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
